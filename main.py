fa0a7721507816fa002ab1e415b03d1603bcc827
Kafi Ashkir <82185473+19133@users.noreply.github.com>
2022-05-25 22:37:27 +0000

    Added comments to explain what each component will do

---
+++ main.py
@@ -0,0 +1,33 @@
+# import statements
+
+
+# functions go here
+
+# ******* Main Routine *******
+
+# Set up dictionaries / list needed to hold data
+
+# Ask user if they have used the program before & show instructions if necessary
+
+# Loop to get ticket details:
+
+  # Get name (Can't be blank)
+
+  # Get age (between 12 and 130)
+  
+  # Calculate ticket price
+
+  # Loop to ask for snacks
+
+  # Calculate snakc price
+
+
+  # Ask for a payment metohd (and apply surcharge if necessary)
+
+
+# Calculate Total sales and profit
+
+# Output data to text file
+
+
+
